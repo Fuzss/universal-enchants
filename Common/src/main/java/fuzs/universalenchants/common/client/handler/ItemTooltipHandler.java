67ee797c1f7039cce200ef1b8af44f78beede594
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-09-03 15:44:51 +0200

    fix tooltip cleanup on NeoForge

--- Common/src/main/java/fuzs/universalenchants/common/client/handler/ItemTooltipHandler.java
+++ Common/src/main/java/fuzs/universalenchants/common/client/handler/ItemTooltipHandler.java
@@ -2,8 +2,8 @@ package fuzs.universalenchants.common.client.handler;
 
 import fuzs.universalenchants.common.handler.ItemCompatHandler;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.PlainTextContents;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.EquipmentSlotGroup;
@@ -11,54 +11,58 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.equipment.Equippable;
 import org.jspecify.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ItemTooltipHandler {
-    /**
-     * Do not use {@link CommonComponents#EMPTY}, this has to be a unique instance.
-     */
-    static final Component PLACEHOLDER_COMPONENT = Component.empty();
 
     public static void onItemTooltip(ItemStack itemStack, List<Component> tooltipLines, Item.TooltipContext tooltipContext, @Nullable Player player, TooltipFlag tooltipFlag) {
-        // armor enchantments are now compatible with player and animals armor slots, attributes therefore are displayed for both slot types
-        // remove the attribute lines again that do not fit the type of armor
-        if (!itemStack.getOrDefault(DataComponents.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.EMPTY)
-                .modifiers()
-                .isEmpty()) {
-            Collection<EquipmentSlotGroup> equipmentSlotGroups = getEquipmentSlotGroupsForRemoval(itemStack);
-            for (EquipmentSlotGroup equipmentSlotGroup : equipmentSlotGroups) {
-                boolean isRemoving = false;
-                for (int i = 0; i < tooltipLines.size(); i++) {
-                    Component component = tooltipLines.get(i);
+        // We make armor enchantments compatible with player and animal armor slots.
+        // Attributes therefore are displayed for both slot types, duplicating the information.
+        // Here we remove the attribute lines again that do not match the equipment slot for this type of armor.
+        if (itemStack.isEnchanted()) {
+            Collection<EquipmentSlotGroup> groups = getEquipmentSlotGroupsForRemoval(itemStack);
+            for (EquipmentSlotGroup equipmentSlotGroup : groups) {
+                boolean removalInProgress = false;
+                ListIterator<Component> iterator = tooltipLines.listIterator();
+                while (iterator.hasNext()) {
+                    Component component = iterator.next();
                     if (component.getContents() instanceof TranslatableContents contents) {
                         if (contents.getKey().equals("item.modifiers." + equipmentSlotGroup.getSerializedName())) {
-                            isRemoving = true;
-                            if (i - 1 >= 0 && tooltipLines.get(i - 1) == CommonComponents.EMPTY) {
-                                tooltipLines.set(i - 1, PLACEHOLDER_COMPONENT);
+                            removalInProgress = true;
+                            iterator.previous();
+                            if (iterator.hasPrevious()) {
+                                Component previousComponent = iterator.previous();
+                                if (previousComponent.getContents() instanceof PlainTextContents previousContents
+                                        && previousContents.text().isEmpty()) {
+                                    iterator.remove();
+                                }
                             }
-                        } else if (!contents.getKey().startsWith("attribute.modifier.")) {
-                            isRemoving = false;
+
+                            iterator.next();
+                        } else if (!contents.getKey().startsWith("attribute.modifier.") && !contents.getKey()
+                                .startsWith("neoforge.modifier.")) {
+                            removalInProgress = false;
                         }
-                        if (isRemoving) {
-                            tooltipLines.set(i, PLACEHOLDER_COMPONENT);
+
+                        if (removalInProgress) {
+                            iterator.remove();
                         }
                     } else {
-                        isRemoving = false;
+                        removalInProgress = false;
                     }
                 }
             }
-            tooltipLines.removeIf((Component component) -> component == PLACEHOLDER_COMPONENT);
         }
     }
 
-    private static Collection<EquipmentSlotGroup> getEquipmentSlotGroupsForRemoval(ItemStack itemStack) {
-        Equippable equippable = itemStack.get(DataComponents.EQUIPPABLE);
+    private static Collection<EquipmentSlotGroup> getEquipmentSlotGroupsForRemoval(ItemStack item) {
+        Equippable equippable = item.get(DataComponents.EQUIPPABLE);
         if (equippable != null && equippable.slot() == EquipmentSlot.BODY) {
             return ItemCompatHandler.ARMOR_EQUIPMENT_SLOT_GROUPS;
         } else {
